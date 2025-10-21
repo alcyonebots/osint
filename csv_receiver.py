ed8b7ec8968201b63786c24c6a29c87af25305ed
alcyonebots <identicate.in@gmail.com>
2025-10-21 18:24:48 +0530

    Update csv_receiver.py

--- csv_receiver.py
+++ csv_receiver.py
@@ -9,11 +9,10 @@ from pyrogram import Client, filters
 from pyrogram.types import Message
 
 # --------------------------- env ---------------------------------------------
-API_ID          = int(os.ge"API_ID"))
-API_HASH        = os.getenv("API_HASH")
-SESSION_STRING  = os.getenv("SESSION_STRING")
-OWNER_ID        = int(os.getenv("OWNER_ID"))   # your user-id (decimal)
-
+API_ID   = 28561722
+API_HASH = "a538ba07656f746def99bed7032121cc"
+OWNER_ID = 8034717776
+SESSION_STRING = "BQGz0ToADlcmxsZ0IAhuXo-4Tl097Ky2F0KRiiqLn-waTjHZSlBifaPX_smVd53hjvi1_PIbwXT3HGzTLBdYKO-7gRo2X0mjJC7p1zhXPcSi4WbXioPhd2RhV97ox1l05rszNfwEmcQPAVwsDaBNV8aBoW_5i8Gj7HUwSiMjEqoLHPxrOdlEJRAxvXmwwkBDD9wO-qtmucMmrnA3dmVTIsghlA1oi_P4l7C_7GwbZqFCv6YzS4x1PTvypazusrTqn86SLogHSaZ2YbTiBsA2CcbhhZsAbqWzj-fCz0qO29KHxAHKiFl6ynUErrg7i6FCXkH_VjpRx3dRpjgvoFs756vAkt6WlAAAAAG_DEnxAA"   # <-- paste here or .env
 # --------------------------- logging -----------------------------------------
 logging.basicConfig(
     level=logging.DEBUG,
